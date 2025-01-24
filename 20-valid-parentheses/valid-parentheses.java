66afbc3f1acff890e38f606251982a872d52e100
Shazan Akhter <101089711+shazanakhter@users.noreply.github.com>
2025-01-24 11:48:46 +0530

    Time: 5 ms (9.27%) | Memory: 42.1 MB (22.57%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -4,11 +4,7 @@ class Solution {
         for(int i=0;i<s.length();i++){
             if(list.size()==0){
                 list.push(s.charAt(i));
-            }else if(list.peek()=='('&&s.charAt(i)==')'){
-                list.pop();
-            }else if(list.peek()=='{'&&s.charAt(i)=='}'){
-                list.pop();
-            }else if(list.peek()=='['&&s.charAt(i)==']'){
+            }else if((list.peek()=='('&&s.charAt(i)==')')||(list.peek()=='{'&&s.charAt(i)=='}')||(list.peek()=='['&&s.charAt(i)==']')){
                 list.pop();
             }else{
                 list.push(s.charAt(i));
