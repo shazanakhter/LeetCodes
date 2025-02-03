83f904bdbf165ada7ba1f2c5b2a077a23fb588e6
Shazan Akhter <101089711+shazanakhter@users.noreply.github.com>
2025-02-03 19:26:15 +0530

    Time: 1 ms (64.84%) | Memory: 46.2 MB (65.44%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,30 +1,35 @@
 class Solution {
-    public int trap(int[] height) {
-        int[] premax=new int[height.length];
-        int[] sufmax=new int[height.length];
-        premax[0]=height[0];
-        sufmax[height.length-1]=height[height.length-1];
+    public int[] prefix(int[] height){
+        int[] prefix=new int[height.length];
+        prefix[0]=height[0];
         for(int i=1;i<height.length;i++){
-            if(premax[i-1]<height[i]){
-                premax[i]=height[i];
-            }else{
-                premax[i]=premax[i-1];
+            prefix[i]=Math.max(prefix[i-1],height[i]);
+        }
+        return prefix;
+    }
+    public int[] suffix(int[] height){
+        int n=height.length;
+        int[] suffix=new int[height.length];
+        suffix[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=Math.max(suffix[i+1],height[i]);
+        }
+        return suffix;
+    }
+    public int trap(int[] height) {
+        int[] pmax=prefix(height);
+        int[] smax=suffix(height);
+        int n=height.length;
+        int amount=0;
+        for(int i=1;i<n-1;i++){
+            int lb=pmax[i-1];
+            int rb=smax[i+1];
+            int yb=Math.min(lb,rb);
+            int contribution=yb-height[i];
+            if(contribution>0){
+                amount+=contribution;
             }
         }
-        for(int i=height.length-2;i>=0;i--){
-            if(sufmax[i+1]<height[i]){
-                sufmax[i]=height[i];
-            }else{
-                sufmax[i]=sufmax[i+1];
-            }
-        }
-        int count=0;
-        for(int i=1;i<height.length-1;i++){
-            int diff=Math.min(premax[i-1],sufmax[i+1]);
-            if(diff-height[i]>0){
-                count=count+diff-height[i];
-            }
-        }
-        return count;
+        return amount;
     }
 }
