43b56fa99d5e7c6a449d745bc17fa937139a3e60
Shazan Akhter <101089711+shazanakhter@users.noreply.github.com>
2025-02-12 21:21:26 +0530

    Time: 1 ms (99.81%) | Memory: 53.2 MB (49.12%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,7 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n=nums.length;
-        Arrays.sort(nums);
-        return nums[n/2];
+        int count=1;
+        int val=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(val==nums[i]){
+                count++;
+            }else{
+                if(count==0){
+                    val=nums[i];
+                }else{
+                    count--;
+                }
+            }
+        }
+        return val;
     }
 }
