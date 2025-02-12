8ca838e0a695e718ccd42c125d826e827e47f346
Shazan Akhter <101089711+shazanakhter@users.noreply.github.com>
2025-02-12 23:23:56 +0530

    Time: 2 ms (99.14%) | Memory: 48.4 MB (18.91%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,21 +1,45 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(hm.containsKey(nums[i])==true){
-                int temp=hm.get(nums[i]);
-                hm.put(nums[i],temp+1);
-            }else{
-                hm.put(nums[i],1);
+        List<Integer> ans=new ArrayList<>();
+        int val1=nums[0];
+        int val2=Integer.MIN_VALUE;
+        int count1=1;
+        int count2=0;
+        for(int i=1;i<nums.length;i++){
+            if(val1==nums[i]){
+                count1++;
+            }else if(val2==nums[i]){
+                count2++;
+            }else if(count1==0){
+                val1=nums[i];
+                count1++;
+            }else if(count2==0){
+                val2=nums[i];
+                count2++;
+            }else {
+                count1--;
+                count2--;
             }
         }
-        int occ=nums.length/3;
-        List<Integer> ans=new ArrayList<>();
-        for(int key:hm.keySet()){
-            if(hm.get(key)>occ){
-                ans.add(key);
+        int c1=0;
+        int c2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==val1){
+                c1++;
             }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==val2){
+                c2++;
+            }
+        }
+        if(c1>nums.length/3){
+            ans.add(val1);
+        }
+        if(c2>nums.length/3){
+            ans.add(val2);
         }
         return ans;
+        
     }
 }
