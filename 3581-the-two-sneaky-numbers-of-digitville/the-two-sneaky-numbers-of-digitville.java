473f6412b57dbba675f1a47053e897b5c883aa5c
Shazan Akhter <101089711+shazanakhter@users.noreply.github.com>
2025-10-31 16:39:35 +0530

    Time: 2 ms (63.09%) | Memory: 46.7 MB (7.71%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -4,8 +4,9 @@ class Solution {
         int count=0;
         HashMap<Integer,Integer> map= new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            if(map.getOrDefault(nums[i],0)>1){
+            int frequency=map.getOrDefault(nums[i],0)+1;
+            map.put(nums[i],frequency);
+            if(frequency>1){
                 ans[count]=nums[i];
                 count++;
             }
