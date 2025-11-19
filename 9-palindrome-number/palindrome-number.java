da83b23a2781d4e28f1c63d18d1bf9512349600b
Shazan Akhter <101089711+shazanakhter@users.noreply.github.com>
2025-11-19 12:48:13 +0530

    Time: 4 ms (100.00%) | Memory: 46.1 MB (6.61%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -3,21 +3,18 @@ class Solution {
         if(x<0){
             return false;
         }
-        boolean ans=false;;
         int temp=x;
         int ans1=0;
         int rem=0;
-        int que=0;
         while(temp>0){
             rem=temp%10;
             ans1=ans1*10+rem;
             temp=temp/10;
         }
         if(ans1==x){
-            ans=true;
-            return ans;
+            return true;
             }
-        return ans; 
+        return false; 
     }
    
 }
