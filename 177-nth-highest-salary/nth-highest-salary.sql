81f05e6cc15a059e0e9a5054aa196907073894c3
Shazan Akhter <101089711+shazanakhter@users.noreply.github.com>
2026-02-13 19:53:38 +0530

    Time: 472 ms (26.19%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,8 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET n=n-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT n,1
+  );
+END
