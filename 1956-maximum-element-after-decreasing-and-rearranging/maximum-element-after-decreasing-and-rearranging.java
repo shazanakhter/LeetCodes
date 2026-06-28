2e39948569fcfba14a27111f6d090e4149572743
Shazan Akhter <101089711+shazanakhter@users.noreply.github.com>
2026-06-28 23:59:08 +0530

    Time: 3 ms (99.58%) | Memory: 79.1 MB (5.00%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        Arrays.sort(arr);
+        /*Arrays.sort(arr);
         arr[0]=1;
         for(int i=1;i<arr.length;i++){
             if(Math.abs(arr[i]-arr[i-1])>1){
@@ -9,6 +9,18 @@ class Solution {
                 continue;
             }
         }
-        return arr[arr.length-1];
+        return arr[arr.length-1];*/
+        int[] freq=new int[arr.length+1];
+        for(int a:arr){
+            int idx=Math.min(a,arr.length);
+            freq[idx]++;
+        }
+        int ans=1;
+        for(int i=2;i<freq.length;i++){
+           int curr=ans+freq[i];
+           ans=Math.min(curr,i);
+        }
+
+        return ans;
     }
 }
