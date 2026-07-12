4f753bfefefdcaaa592f2d9a7a4abc3d372fe657
Shazan Akhter <101089711+shazanakhter@users.noreply.github.com>
2026-07-12 23:59:23 +0530

    Time: 3 ms (54.34%) | Memory: 44.6 MB (46.56%) - LeetSync

--- 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.java
+++ 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.java
@@ -1,40 +1,13 @@
 class Solution {
     public int secondsBetweenTimes(String startTime, String endTime) {
-        int totalstart=0;
-        int totalend=0;
-        int start=0;
-        int end=0;
-        for(int i=0;i<2;i++){
-            int tempStart=startTime.charAt(i)-'0';
-            int tempEnd=endTime.charAt(i)-'0';
-            start=start*10+tempStart;
-            end=end*10+tempEnd;
-        }
-        totalstart=start*3600;
-        totalend=end*3600;
-        start=0;
-        end=0;
+        return seconds(endTime.split(":"))-seconds(startTime.split(":"));
+    }
 
-        for(int i=3;i<5;i++){
-            int tempStart=startTime.charAt(i)-'0';
-            int tempEnd=endTime.charAt(i)-'0';
-            start=start*10+tempStart;
-            end=end*10+tempEnd;
-        }
-        totalstart+=start*60;
-        totalend+=end*60;
-        start=0;
-        end=0;
-        for(int i=6;i<8;i++){
-            int tempStart=startTime.charAt(i)-'0';
-            int tempEnd=endTime.charAt(i)-'0';
-            start=start*10+tempStart;
-            end=end*10+tempEnd;
-        }
-        totalstart+=start;
-        totalend+=end;
-        System.out.println(totalend+" "+totalstart);
-        int diff=totalend-totalstart; 
-        return diff;
+    public int seconds(String[] str){
+        int cnt=0;
+        cnt+=Integer.parseInt(str[0])*60*60;
+        cnt+=Integer.parseInt(str[1])*60;
+        cnt+=Integer.parseInt(str[2]);
+        return cnt;
     }
 }
